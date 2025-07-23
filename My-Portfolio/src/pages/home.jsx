626f865c611838b5c58498cfbb578e353dd30f6f
Schanze117 <aaronschanzenbach@gmail.com>
2025-07-22 19:27:29 -0600

    updated home page and portfolio items.

--- My-Portfolio/src/pages/home.jsx
+++ My-Portfolio/src/pages/home.jsx
@@ -3,11 +3,11 @@ export default function home() {
         <div className="home">
             <h1>Welcome to My Portfolio</h1>
             <p>
-                I'm Aaron Schanzenbach, I am new to the coding world; however i believe by combining my previous work experience with my newfound knowledge I have a lot to offer it!
-                I and my family reside in northern Wyoming, where I am currently enrolled in a Full-Stack Developer bootcamp. 
-                I have a strong foundation in both front-end and back-end technologies at this point in my coding training, and I am always eager to learn and adapt to new challenges and technologies.
+                I'm Aaron Schanzenbach, a Full Stack Developer new to the coding world; however I believe by combining my previous work experience with my newfound knowledge I have a lot to offer it!
+                I and my family reside in northern Wyoming, where I recently graduated from a Full-Stack Developer bootcamp.
+                I have a strong foundation in both front-end and back-end technologies and I am always eager to learn and adapt to new challenges and technologies.
 
-                Please feel free to take a look at some of my projects, learn a little more about me, and contact me if you have any questions or opportunities!
+                Please take a look at some of my projects, learn a little more about me, and contact me if you have any questions or opportunities!
             </p>
         </div>
     );
--- My-Portfolio/src/pages/portfolio.jsx
+++ My-Portfolio/src/pages/portfolio.jsx
@@ -1,8 +1,8 @@
 import Project from '../components/projects';
 import project1Image from '../assets/images/PopUpTrivia.png';
 import project2Image from '../assets/images/READMEGenerator.png'; 
-import project3Image from '../assets/images/VehicleGenerator.png';
-import project4Image from '../assets/images/EmployeeTracker.png';
+import project3Image from '../assets/images/respawnRoom.png';
+import project4Image from '../assets/images/weatherApp.png';
 import project5Image from '../assets/images/KanbanBoard.png';
 import project6Image from '../assets/images/CandidateSearch.png';
 
@@ -19,6 +19,13 @@ export default function Portfolio() {
                     linkRepo="https://github.com/Schanze117/PopUpTrivia"
                     linkApp="https://schanze117.github.io/PopUpTrivia/" 
                 />
+               <Project 
+                        title="Respawn Room"
+                        description="Respawn Room is your all-in-one gaming hub, designed to streamline the way you discover, organize, and manage your gaming collection."
+                        image={project3Image}
+                        linkRepo="https://github.com/Schanze117/RespawnRoom"
+                        linkApp="https://respawnroom-client.onrender.com/"
+              />
                 <Project 
                     title="README Generator"
                     description="This is a command-line application that generates a professional README.md file for your projects. It prompts the user for information about the project and then creates a README file with the provided details. This tool is built using Node.js and Inquirer.js."
@@ -27,33 +34,13 @@ export default function Portfolio() {
                     linkApp="https://drive.google.com/file/d/196cwaqvRTrv2MoHOTkYoDcW1gnkqwtbf/view?usp=drive_link"
                 />
                 <Project 
-                    title="Vehicle Builder"
-                    description="This is a command line application that allows users to build and customize their own vehicles. It prompts the user for various options such as make, model, color, and features, and then generates a summary of the vehicle. This project was built using Node.js and Inquirer.js, and it demonstrates my ability to create interactive command-line applications."
-                    image={project3Image}
-                    linkRepo="https://github.com/Schanze117/VehicleBuilder"
-                    linkApp="https://drive.google.com/file/d/1s9AJdhUIs1HVhNY2MMi2ySAxnTVUdRIW/view?usp=drive_link"
-                />
-                <Project 
-                    title="Employee Tracker"
-                    description="This is a command-line application that allows users to manage a company's employee database. It provides functionality to add, view, update, and delete employees, roles, and departments. The application uses postgreSQL for the database and Inquirer.js for user prompts. This project showcases my ability to work with databases and create interactive command-line applications."
+                    title="Weather App"
+                    description="This is a web application that allows users to check the weather in their area. It uses the OpenWeatherMap API to fetch weather data and displays it in a user-friendly interface. Users can search for their city and view the current weather, as well as a 5-day forecast. This project showcases my ability to work with APIs and build responsive web applications."
                     image={project4Image}
-                    linkRepo="https://github.com/Schanze117/EmployeeTracker"
-                    linkApp="https://drive.google.com/file/d/1o_uMxnNNdWCfwEa3Z_sKc15fE6Uzvjaz/view?usp=sharing"
+                    linkRepo="https://github.com/Schanze117/WeatherApp"
+                    linkApp="https://weatherapp-pok6.onrender.com/"
                 />
-                <Project 
-                    title="Kanban Board"
-                    description="This is a web application that allows users to create and manage tasks using a Kanban board interface. Users can add, edit, and delete tasks, as well as move them between different columns (To Do, In Progress, Done). This project was built using React.js for the frontend and Node.js with Express for the backend. It demonstrates my skills in full-stack development and working with RESTful APIs."
-                    image={project5Image}
-                    linkRepo="https://github.com/Schanze117/Kanban-Board"
-                    linkApp="https://kanban-board-q3qp.onrender.com/"
-                />
-                <Project 
-                    title="Candidate Search Tool"
-                    description="This is a web application designed to help recruiters and hiring managers find suitable candidates for job openings by searching GitHub. It will randomly select candidates from GitHub and give the option to save that candidate or skip to the next one. The application uses the GitHub API to search for candidates based on specific criteria such as skills, location, and experience. It also allows users to save their favorite candidates for later review. This project showcases my ability to work with APIs and build user-friendly web applications."
-                    image={project6Image}
-                    linkRepo="https://github.com/Schanze117/CandidateSearch"
-                    linkApp="https://candidatesearch-1-skuq.onrender.com/"
-                />
+        
             </div>
         </div>
     );
