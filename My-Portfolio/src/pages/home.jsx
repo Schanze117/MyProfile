69638c560d1e09654c91e3783ef8dd1b30f16b8f
Schanze117 <aaronschanzenbach@gmail.com>
2025-07-28 19:29:02 -0600

    made the application responsive on mobile

--- My-Portfolio/src/pages/home.jsx
+++ My-Portfolio/src/pages/home.jsx
@@ -3,7 +3,7 @@ export default function home() {
         <div className="home">
             <h1>Welcome to My Portfolio</h1>
             <p>
-                I'm Aaron Schanzenbach, a Full Stack Developer new to the coding world; however I believe by combining my previous work experience with my newfound knowledge I have a lot to offer it!
+                I'm Aaron Schanzenbach, a Full Stack Developer new to the coding world; however I believe by combining my previous work experience with my newfound knowledge I have a lot to offer!
                 I and my family reside in northern Wyoming, where I recently graduated from a Full-Stack Developer bootcamp.
                 I have a strong foundation in both front-end and back-end technologies and I am always eager to learn and adapt to new challenges and technologies.
 
